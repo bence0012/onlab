35990f40644630d879d93f472dad750144575c17
bence0012 <bence7123@gmail.com>
2023-05-18 16:32:13 +0200

    Room fully operational

--- Assets/procGen.cs
+++ Assets/procGen.cs
@@ -27,7 +27,7 @@ public class Door
     public bool generate;
     public int whichWall;
     public Vector3 pos;
-    public Vector3 rot=new Vector3();
+    public Vector3 rot=new Vector3(1,1,1);
     public Vector3 scale;
     public GameObject obj;
     
@@ -57,9 +57,7 @@ public class procGen : MonoBehaviour
     public Vector2 groundDividers;
 
 
-    [Header("Colliders")]
-    public BoxCollider[] wallColliders;
-    public BoxCollider groundCollider;
+    public PhysicMaterial physicMaterial;
 
     [Header("Additional elements")]
     public GameObject doorPrefab;
@@ -73,13 +71,24 @@ public class procGen : MonoBehaviour
     List<Matrix4x4> walls;
     List<Matrix4x4> grounds;
 
+    List<BoxCollider> wallColliders;
+    BoxCollider groundCollider;
+
     // Start is called before the first frame update
     void Start()
     {
         doors = new List<Door>();
-        
-        
-        
+        groundCollider = gameObject.AddComponent<BoxCollider>();
+        groundCollider.material = physicMaterial;
+
+        wallColliders = new List<BoxCollider>();
+        for (int i = 0; i < 8; i++)
+        {
+            wallColliders.Add(gameObject.AddComponent<BoxCollider>());
+            wallColliders[i].material = physicMaterial;
+
+        }
+
 
     }
 
@@ -128,25 +137,25 @@ public class procGen : MonoBehaviour
         }
         for (int i = 0; i < numOfWallsX; i++)
         {
-            if (i == doors[1].wallId)
+            if (i == doors[1].wallId && doors[1].generate)
             {
                 doors[1].pos = transform.position + new Vector3(-roomLength.x / 2 + wallSize * scaleX / 2 + i * scaleX * wallSize, 0, -roomLength.y / 2-1.8f);
                 doors[1].rot = new Vector3(0, 0, -1);
-                doors[1].scale = new Vector3(scaleX, 1, 1);
+                doors[1].scale = new Vector3(scaleX * 1.01f, 1, 1);
 
                 continue;
             }
             var t = transform.position + new Vector3(-roomLength.x / 2 + wallSize * scaleX / 2 + i * scaleX * wallSize, 0, -roomLength.y / 2);
             var r = transform.rotation;
             r.SetLookRotation(new Vector3(0,0,-1));
-            var s = new Vector3(scaleX* 1.01f, 1, 1);
+            var s = new Vector3(scaleX, 1, 1);
 
             var mat = Matrix4x4.TRS(t, r, s);
             walls.Add(mat);
         }
         for (int i = 0; i < numOfWallsY; i++)
         {
-            if (i == doors[2].wallId)
+            if (i == doors[2].wallId && doors[2].generate)
             {
                 doors[2].pos = transform.position + new Vector3(-roomLength.x / 2 - 1.8f, 0, -roomLength.y / 2 + wallSize * scaleY / 2 + i * scaleY * wallSize);
                 doors[2].rot = new Vector3(-1, 0, 0);
@@ -165,7 +174,7 @@ public class procGen : MonoBehaviour
         }
         for (int i = 0; i < numOfWallsY; i++)
         {
-            if (i == doors[3].wallId)
+            if (i == doors[3].wallId && doors[3].generate)
             {
                 doors[3].pos = transform.position + new Vector3(roomLength.x / 2 + 1.8f, 0, -roomLength.y / 2 + wallSize * scaleY / 2 + i * scaleY * wallSize);
                 doors[3].rot = new Vector3(1, 0, 0);
@@ -224,17 +233,26 @@ public class procGen : MonoBehaviour
 
     void CalculateColliders()
     {
-        wallColliders[0].center = new Vector3(roomLength.x / 2 + 0.05f, 0, 0);
-        wallColliders[0].size = new Vector3(meshes[0].bounds.size.z, meshes[0].bounds.size.y, roomLength.y);
-        wallColliders[1].center = new Vector3(-roomLength.x / 2 - 0.05f, 0, 0);
-        wallColliders[1].size = new Vector3(meshes[0].bounds.size.z, meshes[0].bounds.size.y, roomLength.y);
-        wallColliders[2].center = new Vector3(0,0, roomLength.y / 2 + 0.05f);
-        wallColliders[2].size = new Vector3(roomLength.x, meshes[0].bounds.size.y, meshes[0].bounds.size.z);
-        wallColliders[3].center = new Vector3(0,0, -roomLength.y / 2 - 0.05f);
-        wallColliders[3].size = new Vector3(roomLength.x,meshes[0].bounds.size.y, meshes[0].bounds.size.z);
+
+        PutCollider(wallColliders[0],new Vector3(roomLength.x / 2 + 0.05f, 0, (doors[3].wallId + 1)*roomLength.y/numOfWallsY/2), new Vector3(meshes[0].bounds.size.z, meshes[0].bounds.size.y, roomLength.y / numOfWallsY * (numOfWallsY - doors[3].wallId-1)));
+        PutCollider(wallColliders[1],new Vector3(roomLength.x / 2 + 0.05f, 0, (numOfWallsY- doors[3].wallId)*-roomLength.y/numOfWallsY/2), new Vector3(meshes[0].bounds.size.z, meshes[0].bounds.size.y, roomLength.y / numOfWallsY * (doors[3].wallId)));
+        PutCollider(wallColliders[2], new Vector3(-roomLength.x / 2 - 0.05f, 0, (doors[2].wallId + 1) * roomLength.y / numOfWallsY / 2), new Vector3(meshes[0].bounds.size.z, meshes[0].bounds.size.y, roomLength.y / numOfWallsY * (numOfWallsY - doors[2].wallId - 1)));
+        PutCollider(wallColliders[3], new Vector3(-roomLength.x / 2 - 0.05f, 0, (numOfWallsY - doors[2].wallId) * -roomLength.y / numOfWallsY / 2), new Vector3(meshes[0].bounds.size.z, meshes[0].bounds.size.y, roomLength.y / numOfWallsY * (doors[2].wallId)));
+        PutCollider(wallColliders[4], new Vector3((doors[0].wallId + 1) * roomLength.x / numOfWallsX / 2,0,roomLength.y / 2 + 0.05f), new Vector3(roomLength.x / numOfWallsX * (numOfWallsX - doors[0].wallId - 1), meshes[0].bounds.size.y, meshes[0].bounds.size.z));
+        PutCollider(wallColliders[5], new Vector3((numOfWallsX - doors[0].wallId) * -roomLength.x / numOfWallsX / 2,0,roomLength.y / 2 + 0.05f), new Vector3(roomLength.x / numOfWallsX * (doors[0].wallId), meshes[0].bounds.size.y, meshes[0].bounds.size.z));
+        PutCollider(wallColliders[6], new Vector3((doors[1].wallId + 1) * roomLength.x / numOfWallsX / 2, 0, -roomLength.y / 2 - 0.05f), new Vector3(roomLength.x / numOfWallsX * (numOfWallsX - doors[1].wallId - 1), meshes[0].bounds.size.y, meshes[0].bounds.size.z));
+        PutCollider(wallColliders[7], new Vector3((numOfWallsX - doors[1].wallId) * -roomLength.x / numOfWallsX / 2, 0, -roomLength.y / 2 - 0.05f), new Vector3(roomLength.x / numOfWallsX * (doors[1].wallId), meshes[0].bounds.size.y, meshes[0].bounds.size.z));
 
         groundCollider.center = new Vector3(0, -meshes[0].bounds.size.y/2-0.01f, 0);
         groundCollider.size=new Vector3(roomLength.x, meshes[1].bounds.size.y, roomLength.y);
+    }
+
+
+    void PutCollider(BoxCollider wallCollider, Vector3 center, Vector3 size)
+    {
+        
+        wallCollider.center = center;
+        wallCollider.size = size;
     }
 
     bool RandomBool()
@@ -269,11 +287,14 @@ public class procGen : MonoBehaviour
                 door.wallId = 0;
                 door.pos = new Vector3();
                 door.generate = RandomBool();
+
                 doors.Add(door);
                 if (door.whichWall < 1)
                     door.wallId = UnityEngine.Random.Range(0, numOfWallsX);
                 else
                     door.wallId = UnityEngine.Random.Range(0, numOfWallsY);
+                if (!door.generate)
+                    door.wallId = -1;
             }
 
 
